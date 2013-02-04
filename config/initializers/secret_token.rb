dba7af29af1a93f850a2ab36abd9465cf4c02460
christinechu <ubuntu@ubuntu-VirtualBox.(none)>
2013-02-04 22:40:27 +0100

    Firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Killapp::Application.config.secret_token = '06d539a5aeee08b78b9e8f7e54fcd6a54605a6f20b26189f552348fc937cfe00b2d600182550c12148ec991bc8292babfb1661f0aad93d2fe9a8b3c6167be991'
